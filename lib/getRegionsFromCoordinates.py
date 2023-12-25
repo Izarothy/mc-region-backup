0c4124abe827ce624f469195139e82e94f1a03a8
Izarothy <izarothmail@gmail.com>
2023-12-25 16:40:40 +0100

    feat: add getregionfromcoordinates

---
+++ lib/getRegionsFromCoordinates.py
@@ -0,0 +1,15 @@
+from roundRegionName import roundRegionName
+
+def getRegionsFromCoordinates(cornerOne: list[int], cornerTwo: list[int]) -> list[str]:
+  [lowestX, lowestZ] = cornerOne
+  [highestX, highestZ] = cornerTwo
+
+  bottomLeftRegion = [roundRegionName(lowestX), roundRegionName(lowestZ)]
+  topRightRegion = [roundRegionName(highestX), roundRegionName(highestZ)]
+
+  allRegions = []
+
+  for x in range(bottomLeftRegion[0], (topRightRegion[0] + 1) ):
+    for z in range(bottomLeftRegion[1], (topRightRegion[1] + 1 )):
+      allRegions.append(f"r.{x},{z}.mca")
+  
