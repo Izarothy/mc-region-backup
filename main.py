241a3a8abfa29c222822df8fbd2744ddd420b3f1
Izarothy <izarothmail@gmail.com>
2023-12-26 16:29:16 +0100

    feat: remove ui

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from ftplib import FTP
 from dotenv import load_dotenv
 import os
-import tkinter as tk
 import zipfile
 from lib.getRegionsFromCoordinates import getRegionsFromCoordinates
 
@@ -18,18 +17,15 @@ password = os.getenv("FTP_PASSWORD")
 # ftp setup
 ftp = FTP(host)  
 ftp.login(login, password)                     
-ftp.cwd('world/DIM100/region')        
 
-def doBackup(cornerOneEntry: str, cornerTwoEntry: str, zipFileNameEntry: str, ):
-  cornerOne = cornerOneEntry.get().strip().split(',')
-  cornerTwo = cornerTwoEntry.get().strip().split(',')
-
-  allRegions = getRegionsFromCoordinates([int(x) for x in cornerOne], [int(x) for x in cornerTwo])
+def doBackup(cornerOne: list[int], cornerTwo: list[int], zipFileName: str, ):
+  ftp.cwd('world/DIM100/region')        
+  allRegions = getRegionsFromCoordinates(cornerOne, cornerTwo)
 
   if (os.getcwd().find('backups') == -1):
     os.chdir('backups')
 
-  with zipfile.ZipFile(f"{zipFileNameEntry.get()}.zip", 'w') as zipFile:
+  with zipfile.ZipFile(f"{zipFileName.get()}.zip", 'w') as zipFile:
     for region in allRegions:
       with open(region, 'wb') as fp:
         ftp.retrbinary(f"RETR {region}", fp.write)
@@ -37,32 +33,4 @@ def doBackup(cornerOneEntry: str, cornerTwoEntry: str, zipFileNameEntry: str, ):
         zipFile.write(region)
         os.remove(region)
   
-  cornerOneEntry.delete(0, 'end')
-  cornerTwoEntry.delete(0, 'end')
-  zipFileNameEntry.delete(0, 'end')
-
-
-# gui
-window = tk.Tk()
-window.geometry('500x300')
-
-labelBottomLeft = tk.Label(text="Bottom left corner")
-entryBottomLeft = tk.Entry()
-labelBottomLeft.pack()
-entryBottomLeft.pack()
-
-labelTopRight = tk.Label(text="Top right corner")
-entryTopRight = tk.Entry()
-labelTopRight.pack()
-entryTopRight.pack()
-
-labelZipfileName = tk.Label(text="Name of the ZIP file")
-entryZipfileName = tk.Entry()
-labelZipfileName.pack()
-entryZipfileName.pack()
-
-button = tk.Button(text="Do backup", command=lambda: doBackup(entryBottomLeft, entryTopRight, entryZipfileName))
-button.pack()
-window.mainloop()
-
 ftp.quit()
