3521a445bf62e3ddd2bf613e4240dec8c9ed4d5b
Izarothy <izarothmail@gmail.com>
2024-01-13 20:07:18 +0100

    fix negative coordinates in region rounding

--- lib/roundRegionName.py
+++ lib/roundRegionName.py
@@ -1,8 +1,8 @@
 from math import floor, ceil
+
 def roundRegionName(coordinate: int) -> int:
   if (coordinate < 0):
-    if (coordinate <= -512):
-      return -1
-    return ceil(coordinate / 512) 
+    if (coordinate >= -512):
+      return ceil(coordinate / 512) 
   
   return floor(coordinate / 512)
